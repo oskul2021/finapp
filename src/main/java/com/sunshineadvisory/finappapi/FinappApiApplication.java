dc4d4caa6f7fbf78020ffa00fbf2445962ed2f19
oskul2021 <albertlionelmalang@gmail.com>
2024-09-18 16:36:59 +0100

    first commit

---
+++ src/main/java/com/sunshineadvisory/finappapi/FinappApiApplication.java
@@ -0,0 +1,10 @@
+package com.sunshineadvisory.finappapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinappApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinappApiApplication.class, args
---
+++ src/test/java/com/sunshineadvisory/finappapi/FinappApiApplicationTests.java
@@ -0,0 +1,10 @@
+package com.sunshineadvisory.finappapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinappApiApplicationTests {
+
+    @Test
+    void contextLoads() {
